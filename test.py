721506092e8b8f10be4327a6b5ac09ed71eb6254
Hari Krishna <Hari Krishna>
2016-11-10 14:28:01 +0300

    initial file with basic left and right movement and shooting (of cats, not bullets)

--- test.py
+++ test.py
@@ -1,6 +1,22 @@
+#    15-112: Principles of Programming and Computer Science
+#    Final Project
+#    Name      : Hari Krishna
+#    AndrewID  : hkrishn1
+
+#    File Created: November 7, 2016
+#    Modification History:
+#    Start              End
+#    7/11/16 9:30pm    7/11/16 10:30pm
+#    8/11/16 2:00pm    8/11/16 5:30pm
+#    9/11/16 8:00pm    9/11/16 9:00pm
+#    10/11/16 3:00am   
+
+#importing needed libraries
 import pygame,sys
 from pygame.locals import *
 
+
+#Class for the Player
 class Player():
     def __init__(self,surf,xpos,ypos):
         self.image=pygame.image.load("cat1.png").convert_alpha()
@@ -19,6 +35,7 @@ class Player():
     def draw(self,surface):
         self.surface.blit(self.image,(self.x,self.y))
 
+#Class for the bullet which inherits the Player Class
 class Weapon(Player):
     def __init__(self,surf,xpos,ypos,bg,wxpos,wypos):
         Player.__init__(self,surf,xpos,ypos)
@@ -35,14 +52,14 @@ class Weapon(Player):
             self.surface.blit(self.wimage,(self.wx,self.wy))
             self.wy-=dist
             pygame.display.update()
+
 pygame.init()
 
 FPS=30
 fpsClock=pygame.time.Clock()
 
 DISPLAYSURF=pygame.display.set_mode((577,472),0,32)
-pygame.display.set_caption('Animation')
-
+pygame.display.set_caption('Animation')
 
 WHITE = (255, 255, 255)
 background=pygame.image.load("background.png")
@@ -62,9 +79,7 @@ while True:
         elif event.type==MOUSEBUTTONDOWN:
             weapon=Weapon(DISPLAYSURF,player.x,player.y,background,player.x+25,player.y)
             player.draw(DISPLAYSURF)
-            pygame.display.update()
             weapon.Shoot()
-            pygame.display.update()
 
             
 
